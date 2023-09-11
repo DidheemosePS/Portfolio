c5ec188b6653701b445c36d6b3173b88c44c6f1f
DidheemosePS <sebastiandethemos2002@gmail.com>
2023-09-11 21:23:17 +0530

    modified:   app/components/navigationSlider.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-[calc(100vh-3rem)] relative overflow-hidden">
-      <div className="w-fit h-full flex flex-col justify-center">
+    <div className="w-full h-fit min-h-[calc(100vh-3rem)] relative overflow-hidden">
+      <div className="w-fit h-full flex flex-col justify-center absolute left-0 z-10">
         <p className="text-xl font-medium lg:text-4xl">Frontend Developer</p>
         <div>
           <p className="text-4xl font-bold md:inline lg:text-5xl">Hi, I'm</p>
@@ -14,7 +14,7 @@ export default function Home() {
         </div>
         <p className="text-4xl font-bold lg:text-5xl">From India</p>
       </div>
-      <div className="w-full max-w-30 h-full max-h-40 absolute bottom-0 -right-32 -z-50 pt-5 sm:-right-32 lg:right-0 xl:max-w-xl-imageWidth-35rem">
+      <div className="w-full h-[calc(100vh-3rem)] absolute -right-32 -z-10 pt-5 sm:w-2/4 sm:right-0">
         <Image
           src="./userImage.png"
           alt="Image"
