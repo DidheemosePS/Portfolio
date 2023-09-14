22353a1e51b82537284d744c662e921ce2c3133b
DidheemosePS <sebastiandethemos2002@gmail.com>
2023-09-14 18:29:03 +0530

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 
-
+const getHomeData = async () => {
+  try {
+    const response = await fetch(`${process.env.VERCEL_URL}/api/home`, {
+      cache: "no-cache",
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default async function Home() {
   interface Data {
@@ -8,7 +17,7 @@ export default async function Home() {
     role: string;
     imageUrl: string;
   }
-  const [data]: Data[] = await [];
+  const [data]: Data[] = await getHomeData();
 
   return (
     <div className="w-full h-fit min-h-[calc(100vh-3rem)] relative overflow-hidden">
