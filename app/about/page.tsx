59cca07e24edb4eca185a37180e777e472ace08e
DidheemosePS <sebastiandethemos2002@gmail.com>
2023-09-17 14:47:04 +0530

    modified:   app/about/page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,16 +10,16 @@ export const metadata: Metadata = {
   description: "About",
 };
 
-// const getAboutData = async () => {
-//   try {
-//     const response = await fetch(`${process.env.SERVER_URL}/api/about`, {
-//       cache: "no-cache",
-//     });
-//     return await response.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const getAboutData = async () => {
+  try {
+    const response = await fetch(`${process.env.SERVER_URL}/api/about`, {
+      cache: "no-cache",
+    });
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default async function About() {
   interface Skills {
@@ -51,52 +51,13 @@ export default async function About() {
     experience: Experience[];
     education: Education[];
   }
-  const [data]: Data[] = [
-    {
-      id: "650366961a8b779e2fff8780",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ducimus quod necessitatibus beatae deleniti, nobis ipsum perferendis sunt tenetur natus inventore et nulla accusantium fugiat, eum eveniet voluptas eligendi doloremque.",
-      imageUrl:
-        "https://utfs.io/f/5bf0f38a-3177-4124-a495-f8aa789b311f_userImage.png",
-      skills: [
-        {
-          id: "650366961a8b779e2fff8781",
-          title: "UI/UX",
-          skill: "Designing Web Interfaces",
-          aboutId: "650366961a8b779e2fff8780",
-        },
-        {
-          id: "650366961a8b779e2fff8782",
-          title: "Web Development",
-          skill: "Web app Development",
-          aboutId: "650366961a8b779e2fff8780",
-        },
-      ],
-      experience: [
-        {
-          id: "650366961a8b779e2fff8783",
-          title: "2023 - Current",
-          experience:
-            "I don't have any experience still I'm looking for oppertunities",
-          aboutId: "650366961a8b779e2fff8780",
-        },
-      ],
-      education: [
-        {
-          id: "650366971a8b779e2fff8784",
-          title: "2020 - 2023",
-          education: "Bachelor of Computer Application",
-          aboutId: "650366961a8b779e2fff8780",
-        },
-      ],
-    },
-  ];
+  const data: Data[] = await getAboutData();
 
   return (
     <div className="w-full h-fit min-h-[calc(100vh-3rem)] flex flex-col items-center gap-5 py-5 lg:flex-row">
       <div className="w-full max-w-xs h-80 rounded-lg sm:max-w-md md:max-w-xl lg:max-w-30 lg:h-25">
         <Image
-          src={data?.imageUrl}
+          src={data[0]?.imageUrl}
           alt="Image"
           width={100}
           height={100}
@@ -109,11 +70,11 @@ export default async function About() {
         <p className="text-center text-2xl font-bold md:text-3xl lg:text-start lg:text-4xl">
           About Me
         </p>
-        <p className="text-justify text-gray-300">{data?.description}</p>
+        <p className="text-justify text-gray-300">{data[0]?.description}</p>
         <AboutButtons>
-          <Skills>{data?.skills}</Skills>
-          <Experience>{data?.experience}</Experience>
-          <Education>{data?.education}</Education>
+          <Skills>{data[0]?.skills}</Skills>
+          <Experience>{data[0]?.experience}</Experience>
+          <Education>{data[0]?.education}</Education>
         </AboutButtons>
       </div>
     </div>
