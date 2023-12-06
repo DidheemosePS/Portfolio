22b3fc1476bc2e3a85fe8828ee0bccdae2f0661d
DidheemosePS <sebastiandethemos2002@gmail.com>
2023-12-06 23:10:52 +0530

    new file:   app/apple-icon.ico 	new file:   app/icon.ico 	modified:   app/layout.tsx 	new file:   app/opengraph-image.png 	deleted:    app/opengraph-image.webp 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import SonnerProvider from "./sonner-provider";
 import Aos from "./aos";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.PORTFOLIO_URL as string),
   title: "Didheemose",
   description:
     "Explore my portfolio—a showcase of my passion and expertise. From design to development, each project tells a unique story. Join me on this journey of creativity and innovation!",
@@ -35,7 +37,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Aos>
-          <SonnerProvider>{children}</SonnerProvider>
+          <SonnerProvider>
+            {children}
+            <Analytics />
+          </SonnerProvider>
         </Aos>
       </body>
     </html>
